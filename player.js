225b6e8ef81ea66195bc75e191c213983bf806c9
itallolegal <38992673+itallolegal@users.noreply.github.com>
2019-12-04 16:21:38 -0300

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,7 @@
 window.addEventListener("message", function(e) {
 
 		//console.log(e);
+		console.log(e.currentTarget.referrer);
 
 		var video_config_media = JSON.parse(e.data.video_config_media);
 		var user_lang = e.data.lang;
