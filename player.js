f275352d6a341aa5b98e98d66b39847e8b545988
itallolegal <38992673+itallolegal@users.noreply.github.com>
2019-12-04 16:41:26 -0300

    Update player.js

--- player.js
+++ player.js
@@ -12,7 +12,7 @@ window.addEventListener("message", function(e) {
 		var video_m3u8= "";
 		var episode_title = "";
 		var series_title = e.currentTarget.document.referrer;
-		var series_url = e.currentTarget.document.referrer.url.substr(0,document.URL.lastIndexOf('/'));
+		var series_url = e.currentTarget.document.referrer.substr(0,document.URL.lastIndexOf('/'));
 		console.log(series_url);
 
 	    for(var i = 0; i < video_config_media['streams'].length; i++)
