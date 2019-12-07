0002bb5c0f9fcc2cf8de0fcb55e23f1056db4cfd
itallolegal <38992673+itallolegal@users.noreply.github.com>
2019-12-07 18:00:37 -0300

    Update player.js

--- player.js
+++ player.js
@@ -55,7 +55,7 @@ window.addEventListener("message", function(e) {
 		}
 		
 		//Pega o titulo da serie
-		$.get(series_rss, function (data) {
+		$.get("https://cors-anywhere.herokuapp.com/" + series_rss, function (data) {
 		    series_title = $(data).find("image").find("title").text();
 		});
 		console.log(series_title);
