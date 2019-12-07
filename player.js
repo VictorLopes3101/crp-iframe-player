b474fe30f64588c4545bf2ba48933d68d8e2d2a0
itallolegal <38992673+itallolegal@users.noreply.github.com>
2019-12-07 17:31:37 -0300

    Update player.js

--- player.js
+++ player.js
@@ -55,7 +55,7 @@ window.addEventListener("message", function(e) {
 		
 		//Pega o titulo da serie
 		series_title = series_url.split('/')[4].replace(/\-/g," ").replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-		console.log(series_title);
+		//console.log(series_title);
 		
 		//Pega o numero e titulo do episodio
 		switch (user_lang[0]) {
