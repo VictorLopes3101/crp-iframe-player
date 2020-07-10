f580558536e3b07a3ba83bd89aa0a59303ef97cd
itallolegal <38992673+itallolegal@users.noreply.github.com>
2020-07-10 14:38:03 -0300

    Update content.js

--- Crunchyroll_Premium/content.js
+++ Crunchyroll_Premium/content.js
@@ -21,7 +21,7 @@ function importPlayer(){
     	console.log("[CR Premium] Adicionando o jwplayer...");
     	ifrm = document.createElement("iframe");
     	ifrm.setAttribute("id", "frame"); 
-		ifrm.setAttribute("src", "https://hyper1025.github.io/crp-iframe-player/"); 
+		ifrm.setAttribute("src", "https://itallolegal.github.io/crp-iframe-player/"); 
 		ifrm.setAttribute("width","100%");
 		ifrm.setAttribute("height","100%");
 		ifrm.setAttribute("frameborder","0");
