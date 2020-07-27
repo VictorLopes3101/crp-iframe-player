55428d2fef88b20b54ef643781d5fdcacec8875d
itallolegal <38992673+itallolegal@users.noreply.github.com>
2020-07-27 15:19:50 -0300

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -214,7 +214,7 @@ window.addEventListener("message", function (e) {
 				//Se o episodio não for apenas para premium pega as urls de um jeito mais facil
 				if(is_ep_premium_only == false) {
 					video_dash_playlist_url_old = player_current_playlist.replace("master.m3u8","manifest.mpd").replace(player_current_playlist.split("/")[2], "dl.v.vrv.co").replace("evs1","evs");
-					video_dash_playlist_url = player_current_playlist.replace(player_current_playlist.split("/")[2], "a-vrv.akamaized.net");
+					video_dash_playlist_url = player_current_playlist.replace(player_current_playlist.split("/")[2], "a-vrv.akamaized.net").replace("evs1","evs");
 
 					//console.log("Dash Playlist: " + video_dash_playlist_url);
 
