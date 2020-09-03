718eca5fa9f5aa5c9404f25e04e5a11db0a00d27
itallolegal <38992673+itallolegal@users.noreply.github.com>
2020-09-03 18:40:51 -0300

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -34,7 +34,7 @@ window.addEventListener("message", function (e) {
 			}
 		}
 		if (video_config_media['streams'][i].format == 'adaptive_hls' && video_config_media['streams'][i].hardsub_lang == user_lang) {
-			video_stream_url = video_config_media['streams'][i].url;
+			video_stream_url = video_config_media['streams'][i].url.replace("pl.crunchyroll.com", "dl.v.vrv.co");
 			break;
 		}
 	}
